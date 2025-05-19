c40679d79f5a538671f428278f8b0f012ee997c8
nietiene <niyomugaboetiene53@gmail.com>
2025-05-19 09:05:57 -0700

    user managment dashboard

---
+++ src/Frontend/frontend.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Frontend = () => {
+    const [user, setUser] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/')
+        .then((res) => {
+            setUser(res.data);
+        }).catch ((err) => setError(err.message));
+    });
+
+ if (err) return <div>{error}</div>
+ if (loading) return <div>Loading.....</div>
+
+ return (
+    <div>
+         <table>
+            <tr>
+
+            
+            <th>id</th>
+            <th>Name</th>
+            <th>Password</th>
+
+            {user.map((user) => (
+                <>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.password}</td>
+                </>
+            ))}
+            </tr>
+         </table>
+    </div>
+ )
+}
