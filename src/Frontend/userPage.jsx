3271fe14ca37d7d788d4cf9a4f4f03777ee47ad2
nietiene <niyomugaboetiene53@gmail.com>
2025-05-21 16:42:35 -0700

    user managment dashboard

--- src/Frontend/userPage.jsx
+++ src/Frontend/userPage.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 const UserPage = () => {
-    const [user, setUser] = useState([]);
+    const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     
@@ -13,7 +13,7 @@ const UserPage = () => {
             try {
                 setLoading(true);
                 const res = await axios.get(`http://localhost:3000/user/${id}`, {withCredentials: true});
-                setUser(res.data);
+                setUser(res.data.user[0]);
                 setLoading(false)
             } catch (err) {
                 setLoading(false);
@@ -25,17 +25,14 @@ const UserPage = () => {
 
   if (error) return <div>{error}</div>
   if (loading) return <div>Loading.....</div>
+  if (!user) return null;
 
 
   return (
-    <div>
-      
-            {user.map((user) => (
-                  <ul key={user.id}>
+    <div>       
+                
                     <li>Name:{user.name}</li>
                     <li>Password:{user.passwrod}</li>
-                  </ul>
-            ))}
        
     </div>
   )
